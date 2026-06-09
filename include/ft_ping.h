fc55dd90a72c20fe3479e197373a6112170f15bd
beddinao <bilaleddinaoui@gmail.com>
2026-06-09 12:25:09 -0400

    domain resolution

--- include/ft_ping.h
+++ include/ft_ping.h
@@ -4,5 +4,10 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,4 +5,24 @@ int main(int c, char **v) {
 	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 	if (socket < 0)
 		printf("error creating the socket\n");
+
+
+	struct addrinfo *dest;
+	struct addrinfo hint;
+
+	memset(&hint, 0, sizeof(hint));
+
+	hint.ai_family = AF_INET;
+	hint.ai_socktype = SOCK_DGRAM;
+	hint.ai_flags = AI_PASSIVE;
+
+
+	int i = getaddrinfo("google.com", NULL, &hint, &dest);
+	if (i) {
+		printf("host unreachable\n");
+		exit(1);
+	}
+	
+	char *ip = inet_ntoa(((struct sockaddr_in*)dest->ai_addr)->sin_addr);
+	printf("google.com: %s\n", ip);
 }
